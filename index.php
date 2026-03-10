661d1bd62b3852b5991c5ac422e22c70ffc31eab
Yasmim <yasmimsousa251@gmail.com>
2026-03-10 16:55:15 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
+    <title>Gestor de Estoque</title>
+</head>
+<body>
+
+    <div class="mb-3" action="Login" style="border;">
+        <form id="formLogin">
+            <label for="emailLogin" class="form-label">Email:</label>
+            <input class="form-control" id="emailLogin" type="email" placeholder="name@example.com">
+            <label for="emailLogin" class="mb-3">Senha:</label>
+            <input type="password" id="senhaLogin" type="password" class="form-control">
+            <div id="mensagemSenha" class="form-text">
+                Senha deve ter ao máximo 8 caracteres, contendo letra e números e pelo menos um caracter especial.
+            </div>
+        </form>
+    </div>
+
+</body>
+</html>
